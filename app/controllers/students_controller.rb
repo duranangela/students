df85700600d89203073d25a399857896d7c2e0e9
Angela Duran <duranangela19@gmail.com>
2018-07-10 14:06:53 -0600

    Add routes and views for show and new pages, plus create

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,4 +4,27 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new(student_params)
+    if @student.save
+      redirect_to student_path(@student)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def student_params
+    params.require(:student).permit(:name)
+  end
+
 end
