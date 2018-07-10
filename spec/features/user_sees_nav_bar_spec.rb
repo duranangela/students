8529969f38bbad89dcc46fca43c3727a78d9a405
Angela Duran <duranangela19@gmail.com>
2018-07-10 14:36:32 -0600

    Add feature test for nav bar on show page

--- spec/features/user_sees_nav_bar_spec.rb
+++ spec/features/user_sees_nav_bar_spec.rb
@@ -1,8 +1,16 @@
 require 'rails_helper'
 
-describe 'user visits student index page' do
-  it 'user sees nav bar' do
+describe 'user visits any page' do
+  it 'user sees nav bar on student index page' do
     visit students_path
+
+    expect(page).to have_selector(:link_or_button,'See All Students')
+    expect(page).to have_selector(:link_or_button, 'Create New Student')
+  end
+  it 'user sees nav bar on student show page' do
+    student = Student.create(name: 'Bobby Brown')
+
+    visit student_path(student)
 
     expect(page).to have_selector(:link_or_button,'See All Students')
     expect(page).to have_selector(:link_or_button, 'Create New Student')
