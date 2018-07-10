edca8fdbe708a2a4f6894019c64791bd20e82b20
Angela Duran <duranangela19@gmail.com>
2018-07-10 15:46:34 -0600

    Add relationship tests for courses, student_courses, and student

---
+++ spec/models/course_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+describe Course, type: :model do
+  describe 'relationships' do
+    it {should have_many(:student_courses)}
+    it {should have_many(:students).through(:student_courses)}
+  end
+end
