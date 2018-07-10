b3d39f9df84ff2719ccb141c4e5f38ba389b9dbc
Angela Duran <duranangela19@gmail.com>
2018-07-10 14:39:32 -0600

    Add feature test for individual student link

--- spec/features/user_sees_all_students_spec.rb
+++ spec/features/user_sees_all_students_spec.rb
@@ -10,4 +10,15 @@ describe 'user sees all students' do
     expect(page).to have_content(student1.name)
     expect(page).to have_content(student2.name)
   end
+  it 'links to individual student show pages' do
+    student = Student.create(name: 'Bobby Jones')
+
+    visit students_path
+
+    click_on student.name
+
+    expect(current_path).to eq(student_path(student))
+    expect(page).to have_content(student.name)
+  end
+
 end
